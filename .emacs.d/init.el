6174a1b10c05f02b2415ce465d7083e24a2274cf
Kevan Hollbach <khollbach@gmail.com>
2018-12-05 21:13:22 -0500

    init.el disable minibuffer startup message

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,10 +2,12 @@
 (blink-cursor-mode 0)
 
 ;; Disable startup messages
+(setq inhibit-startup-screen t)
+(defun display-startup-echo-area-message ()
+  (message ""))
 (setq initial-scratch-message "")
-(setq inhibit-startup-message t)
 
 ;; Disable scroll bar and menu bars
 (scroll-bar-mode 0)
-(tool-bar-mode 0)
 (menu-bar-mode 0)
+(tool-bar-mode 0)
