ab4aade827e62798a61fbc2b0bfa2a8011b06843
Kevan Hollbach <khollbach@gmail.com>
2026-08-16 11:06:32 -0400

    Remove unused settings

--- .lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:flowstorm
- {:dependencies [[com.github.flow-storm/clojure "RELEASE"]
-                 [com.github.flow-storm/flow-storm-dbg "RELEASE"]]
-  :exclusions [org.clojure/clojure]
-  :jvm-opts ["-Dflowstorm.startRecording=false"
-             "-Dclojure.storm.instrumentEnable=true"
-             "-Dclojure.storm.instrumentAutoPrefixes=true"]}}
